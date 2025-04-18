0c037556b91f2b3c9c9f3af162fb7f7433c5ce12
k-arikawa0312 <s1f102402710@iniad.org>
2025-04-18 11:30:13 +0900

    add teamColor

---
+++ src/constants/teamColor.ts
@@ -0,0 +1,14 @@
+export const teamColor = {
+    Giants: '#EB9713',
+    Tigers: '#FFE201',
+    Baystars: '#0091e1',
+    Carp: '#FF0000',
+    Swallows: '#98C145',
+    Dragons: '#002569',
+    Hawks: '#FCC700',
+    Fighters: '#275b91',
+    Marines: "C0C0C0",
+    Eagles: '870010',
+    Buffaloes: '#00011f',
+    Lions: '092048',
+}
