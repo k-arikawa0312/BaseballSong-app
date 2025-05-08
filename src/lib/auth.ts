6e2f6cd96d557b6396186ee12f077a095170055e
k-arikawa0312 <s1f102402710@iniad.org>
2025-05-08 17:14:10 +0900

    betterAuth setting

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../../src/generated/prisma";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
-const prisma = new PrismaClient();
+// グローバルスコープでPrismaClientのインスタンスを宣言
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// 開発環境での重複インスタンス作成を防ぐ
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
