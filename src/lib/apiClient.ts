55443c044e436c3cb8adc50c3c12bccaabaa0880
k-arikawa0312 <s1f102402710@iniad.org>
2025-05-10 04:29:54 +0900

    frourionext setting

---
+++ src/lib/apiClient.ts
@@ -0,0 +1,10 @@
+import { $fc } from "@/app/api/frourio.client";
+
+export const apiClient = $fc({
+    baseURL:process.env.BETTER_AUTH_URL,
+    init:{
+        headers:{
+            'X-Custom-Header': 'value',
+        },
+    },
+})
