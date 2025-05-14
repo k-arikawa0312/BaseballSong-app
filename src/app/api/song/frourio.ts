19e32fcb62b89883ce308b9f1cc576dd11dbc941
k-arikawa0312 <s1f102402710@iniad.org>
2025-05-14 12:02:13 +0900

    GETの追加

--- src/app/api/song/frourio.ts
+++ src/app/api/song/frourio.ts
@@ -1,12 +1,29 @@
-import type { FrourioSpec } from '@frourio/next';
-import { z } from 'zod';
-import { midiFileSchema } from '@/schema/midiFile';
+import type { FrourioSpec } from "@frourio/next";
+import { z } from "zod";
+import { midiFileSchema } from "@/schema/midiFile";
+
+// データベースから取得する曲の型定義
+const SongSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  team: z.string(),
+  midiFile: z.any(), //いつか修正
+  lyrics: z.string(),
+});
+
 export const frourioSpec = {
   post: {
-    format: 'formData',
+    format: "formData",
     body: midiFileSchema,
-    res: { 200: { body: z.object({ value: z.string() }) },
-        500: { body: z.object({ error: z.string() }) },
+    res: {
+      200: { body: z.object({ value: z.string() }) },
+      500: { body: z.object({ error: z.string() }) },
+    },
+  },
+  get: {
+    res: {
+      200: { body: z.object({ songs: z.array(SongSchema) }) },
+      500: { body: z.object({ error: z.string() }) },
     },
   },
 } satisfies FrourioSpec;
