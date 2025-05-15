70cf0f996f46808d13bb7bc380a04f3ef291d22e
k-arikawa0312 <s1f102402710@iniad.org>
2025-05-15 12:05:51 +0900

    midiファイルを追加時に取り出すように修正

--- src/schema/midiFile.ts
+++ src/schema/midiFile.ts
@@ -1,11 +1,27 @@
 import { z } from "zod";
 
-const MIDIFILE_TYPE = ['midi']
+const MIDIFILE_TYPE = ["midi"];
 
 export const midiFileSchema = z.object({
   title: z.string(),
   team: z.string(),
-  midiFile: z.instanceof(File),
+  midiFile: z
+    .any()
+    .refine(
+      (value) =>
+        value instanceof File ||
+        (value instanceof FileList && value.length > 0),
+      {
+        message: "MIDIファイルを選択してください",
+      }
+    )
+    .transform((value) => {
+      if (value instanceof FileList && value.length > 0) {
+        console.log("FileListからFileに変換", value[0]);
+        return value[0];
+      }
+      return value;
+    }),
   lyrics: z.string(),
 });
 
